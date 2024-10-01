a9e7e5940154cf8c43c8bbeb3009f3de1bb921e0
rubel <vxlrubel@gmail.com>
2024-10-02 00:17:59 +0600

    update

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="clearfix">
-    <div class="container">
-      About page content: Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui provident
-      explicabo reprehenderit tempore sed consequuntur quisquam eum quia incidunt! Veritatis culpa
-      perspiciatis enim quasi molestiae!
-    </div>
-  </div>
-</template>
